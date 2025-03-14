89d40ede5d89d0e78a85d8a141edca1747daaa7b
pctp04 <patinopaulchristian@gmail.com>
2025-03-14 04:27:31 -0700

    added question2 comments for explanation

--- question2.js
+++ question2.js
@@ -1,8 +1,10 @@
+//used setTimeout to delay the greeting by 2 seconds
 function delayedGreeting(name) {
     setTimeout(() => {
       console.log(`Hello, ${name}!`);
     }, 2000);
   }
 
+  // call delayedGreeting function with alice
   delayedGreeting("Alice");
   
