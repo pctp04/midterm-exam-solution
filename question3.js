7a828f962f6eb14b33a0ecdd88ffae1873e664f0
pctp04 <patinopaulchristian@gmail.com>
2025-03-14 04:26:44 -0700

    added question3 comments for explanation

--- question3.js
+++ question3.js
@@ -1,10 +1,13 @@
+//import express and create an instance of express
 const express = require ('express');
 const app = express();
 
+//create route for question3
 app.get('/question3',(req,res)=>{
     res.json({message: 'Express is working! write your full name'});
 });
 
+//port 3000 is listening to check if its working
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
